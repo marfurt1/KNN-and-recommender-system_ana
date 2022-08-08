55d27c67c4e76dd70e63fc8646a84e5c86c73166
marfurta <anamariamarfurt@gmail.com>
2022-08-08 00:31:24 +0000

    agredagos

--- src/app.py
+++ src/app.py
@@ -6,8 +6,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import matplotlib inline
 import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Import dataset
 
